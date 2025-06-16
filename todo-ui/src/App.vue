3e853413f8b7c635810944f9d7210ba2e66724f4
csejtei1dav713 <csejteidavid90@gmail.com>
2025-06-16 14:22:24 +0200

    init

---
+++ todo-ui/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Docker frontend + backend 🐳</h1>
+  <p v-if="message">{{ message }}</p>
+</template>
+
+<script setup>
+  import { ref, onMounted } from 'vue'
+
+  const message = ref('')
+
+  onMounted(async () => {
+    const res = await fetch('http://localhost:3000/api/hello')
+    const data = await res.json()
+    message.value = data.message
+  })
+</script>
+
+<style>
+  body {
+    font-family: sans-serif;
+  }
+</style>
